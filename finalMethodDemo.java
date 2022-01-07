dcc8d85c0fbf87db3367f5a7a723b7771f62a1e5
iRachana06 <rachanaarora1992@gmail.com>
2022-01-07 23:38:21 +0530

    Creating a final method in java

---
+++ finalMethodDemo.java
@@ -0,0 +1,18 @@
+class finalDemo {
+    final void display(){System.out.println("This is Demo of using Final Method");}
+    }
+
+   public class finalMethodDemo extends finalDemo{
+//        void display(){
+//          System.out.println("");
+//        }
+        //As display is a Final method, it cant be overriding
+        //If you uncomment lines 6-8,this program will throw Compile time Error
+
+        public static void main(String args[]){
+            finalMethodDemo obj= new finalMethodDemo();
+            obj.display();
+        }
+    }
+
+
