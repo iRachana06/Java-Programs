8f92e217bc288a543dd9f10302afd91c87619931
iRachana06 <rachanaarora1992@gmail.com>
2022-01-08 19:24:51 +0530

    Update finalClassDemo.java

--- finalClassDemo.java
+++ finalClassDemo.java
@@ -1,4 +1,4 @@
-final class ClassDemo {
+class ClassDemo {
     void display(){
         System.out.println("This is Demo of using Final Method");
     }
@@ -7,12 +7,11 @@ public class finalClassDemo extends ClassDemo {
     void display(){
         System.out.println("This is statement from Inherited class");
     }
-            /* As ClassDemo is a Final class, it cant be Inherited
-    If you uncomment lines 7-9,this program will throw Compile time Error
-    Either remove final keyowrd in line 1 or comment lines 7-9*/
+                /* As ClassDemo is a Final class, it cant be Inherited
+        Remove final keyowrd in line 1 otherwise this program will throw Compile time Error*/
 
-            public static void main(String args[]){
-                finalClassDemo obj= new finalClassDemo();
-                obj.display();
-            }
+    public static void main(String args[]){
+        finalClassDemo obj= new finalClassDemo();
+        obj.display();
+    }
 }
