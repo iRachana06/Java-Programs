f765c1f6abc993e382242d850d24cb50803677f7
Rachanaa Arora <68515362+iRachana06@users.noreply.github.com>
2022-03-21 23:10:39 +0530

    Update HashMapDemo.java

--- Advanced Java/HashMapDemo.java
+++ Advanced Java/HashMapDemo.java
@@ -2,13 +2,12 @@ import java.util.*;
 public class HashMapDemo{
     public static void main(String args[]){
         HashMap<Integer,String> map=new HashMap<Integer,String>();//Creating HashMap
-        map.put(1,"Java");  //Put elements in Map
+        map.put(1,"Java");//Put elements in Map
         map.put(2,"Javascript");
         map.put(3,"CSS");
         map.put(4,"Python");
-
         System.out.println("Key and Values in Hashmap are : ");
-        for(Map.Entry element : map.entrySet()){ //Iterating through Hashmap
+        for(Map.Entry element : map.entrySet()){  //Iterating through Hashmap
             System.out.println("Key: " +element.getKey()+" Value : "+element.getValue());
         }
         map.put(4,"Typescript"); // Duplicate Entry will overwrite value associated with Key 4
@@ -16,12 +15,10 @@ public class HashMapDemo{
         HashMap<Integer,String> map2=new HashMap<Integer,String>();//Creating HashMap
         map2.putAll(map); //Create a copy of existing HashMap
         System.out.println("New Hashmap created by putAll: "+map2);
-
-        map.remove(4); //Remove element from Hashmap
-        System.out.println("Updated list after removing element: "+map);
-
+        map.remove(4);//Remove element from Hashmap
         map.replace(2,"SQL");
         map.replace(3, "CSS", "HTML");
+
         System.out.println("Updated list - replacing elements in Hashmap: "+map);
     }
 }
